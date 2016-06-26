510ced1737311572d8e7c93ebf023db8acc1719a
Robin Murphy <robin@robinmurphy.co.uk>
2016-06-26 12:32:58 +0100

    Remove vim colour scheme

--- install.sh
+++ install.sh
@@ -2,10 +2,8 @@
 
 # vim
 mkdir -p ~/.vim
-rm -rf ~/.vim/colors
 
 # symlinks
 ln -sf $(pwd)/git/config ~/.gitconfig
 ln -sf $(pwd)/zsh/zshrc ~/.zshrc
 ln -sf $(pwd)/vim/vimrc ~/.vimrc
-ln -sf $(pwd)/vim/colors ~/.vim/colors
